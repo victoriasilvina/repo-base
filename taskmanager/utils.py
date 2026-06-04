2eceb2b360041c97ad113c474067f0dfe307ecb5
holandaNycollas <holandanycollas@gmail.com>
2026-06-04 14:20:21 -0300

    feat: exibir status de conclusão das tarefas

--- taskmanager/utils.py
+++ taskmanager/utils.py
@@ -1,8 +1,8 @@
 # Zona de conflito intencional: ambos os devs modificarão format_task e filter_tasks
 
 def format_task(task):
-    status = "[ ]"
-    return f"{status} [{task['priority']}] #{task['id']} - {task['title']}"
+    status = "[✓]" if task.get("done", False) else "[ ]"
+    return f"{status} [{task['priority'].upper()}] #{task['id']} - {task['title']}"
 
 def filter_tasks(tasks, show_done=True):
     if show_done:
